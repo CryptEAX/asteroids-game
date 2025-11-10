b4f4ad354f0873fefd8d011b447ae02c0264b64c
CryptEAX <crypteax@proton.me>
2025-11-10 12:17:51 -0500

    Added game window

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+pygame.init()
+
+running = True
+
 def main():
     print(f"Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while running:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
