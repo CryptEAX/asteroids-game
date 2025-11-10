8f4824997dc10eaf2a6b6a9db42cf09a9dcb4e13
CryptEAX <crypteax@proton.me>
2025-11-10 15:31:35 -0500

    Decoupled game speed from fps

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from logger import log_state
 
 pygame.init()
 
+
 running = True
 
 def main():
     print(f"Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while running:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +20,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
