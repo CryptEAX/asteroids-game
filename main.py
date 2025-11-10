117b25311230014c4f654a6dd58c52324aac65e3
CryptEAX <crypteax@proton.me>
2025-11-10 11:07:18 -0500

    Added constants

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
-    print(f"Starting Asteroids with pygame version: {str(pygame.version.ver)}")
+    print(f"Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
