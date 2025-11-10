90fe02e9291762df6728adbb40345aa86f448ad1
CryptEAX <crypteax@proton.me>
2025-11-10 16:03:07 -0500

    Created Player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 pygame.init()
 
@@ -13,15 +14,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x, y = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
+    player_ship = Player(x, y)
     while running:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_ship.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
