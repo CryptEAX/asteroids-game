5acfe02acedd534f8f343e41c4999f2c25f60b11
CryptEAX <crypteax@proton.me>
2025-11-14 15:26:57 -0500

    Added asteroid destruction

--- main.py
+++ main.py
@@ -7,40 +7,58 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
 
-pygame.init()
 
-running = True
+
+
 
 def main():
     print(f"Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     dt = 0
     x, y = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
+
     updatable, drawable, asteroids, shots = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     Shot.containers = (shots, updatable, drawable)
+
     player_ship = Player(x, y)
-    asfield = AsteroidField()
+    asteroid_field = AsteroidField()
+
     while running:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+
         updatable.update(dt)
-        for a in asteroids:
-            if a.collides_with(player_ship):
+        for asteroid in asteroids:
+            if asteroid.collides_with(player_ship):
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+
         for d in drawable:
             d.draw(screen)
+
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
